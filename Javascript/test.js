d838f3a9cfd47c5b7496708bdf517a3ad065a501
volodymyryakymovych <yakimovuch@gmail.com>
2022-04-14 15:25:31 +0300

    add

---
+++ Javascript/test.js
@@ -0,0 +1,31 @@
+// let a = 100;
+// let b = 500;
+// let c = "hello";
+// let d = "okten";
+// const x = 'constant value';
+// {
+//     console.log(a);
+//     x = 'new value';
+// }
+// let b = 'new value';
+//
+// let result = 100 + y;
+//
+// console(result);
+
+// CORRECT
+//
+// let a = 100;
+// let b = 500;
+// let c = "hello";
+// let d = "okten";
+// const x = 'constant value';
+//
+// console.log(a);
+// let y = 1;
+// b = 100;
+//
+// let result = 100 + y;
+//
+// console.log(result);
+
